f5f57142fc84b1e36dd4e33740792c419d51552f
snoopfear <39308472+snoopfear@users.noreply.github.com>
2025-03-09 17:18:06 +0300

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -2,4 +2,4 @@ cd $HOME/nearcore && NEWTAG=$(curl -s https://api.github.com/repos/near/nearcore
 
 cd $HOME/nearcore && make release
 
-cd $HOME && sudo systemctl stop neard && rm /usr/local/bin/neard && cp "$HOME/nearcore/target/release/neard" /usr/local/bin/neard && sudo systemctl restart neard && echo "Текущая версия NEAR:" && neard --version && journalctl -n 100 -f -u neard | ccze -A
+cd $HOME && sudo systemctl stop neard && rm /usr/local/bin/neard && cp "$HOME/nearcore/target/release/neard" /usr/local/bin/neard && sudo systemctl restart neard && echo "Текущая версия NEAR:" && neard --version && journalctl -n 100 -f -u neard | ccze -A | grep INFO
